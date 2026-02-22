1c74b45854071fb7bd5e14d511a23f0658ef706e
rimon22 <145893943+ririmon22@users.noreply.github.com>
2026-02-22 08:22:04 +0000

    feat: ver0.2 operator extension and safety checks

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,41 @@ fn main() {
     use std::io;
 
     let mut input = String::new();
+    let operator;
+    let parts: Vec<&str>;
     io::stdin().read_line(&mut input).expect("入力に失敗しました");
     let input = input.trim();
-    let parts: Vec<&str> = input.split('+').collect();
+    if input.contains('+') {
+        operator='+';
+        parts = input.split('+').collect();
+    }else if input.contains('-') {
+        operator='-';
+        parts = input.split('-').collect();
+    }else if input.contains('*') {
+        operator='*';
+        parts = input.split('*').collect();
+    }else if input.contains('/') {
+        operator='/';
+        parts = input.split('/').collect();
+    }else {
+        println!("対応していない演算子です。");
+        return;
+    }
+    
+    if parts.len() != 2 {
+        println!("2項演算のみに対応しています。");
+        return;
+    }
     let n1 = parts[0].parse::<i32>().unwrap();
     let n2 = parts[1].parse::<i32>().unwrap();
-    println!("{}",n1+n2);
     
+    let result = match operator {
+        '+' => n1+n2,
+        '-' => n1-n2,
+        '*' => n1*n2,
+        '/' => n1/n2,
+        _ => unreachable!(),
+    };
 
-    //TODO: 式を振り分ける
-    //TODO: 計算する
-    //TODO: 結果を表示する
+    println!("計算結果：{}", result);
 }
